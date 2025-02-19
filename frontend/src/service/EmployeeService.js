06ecc28865470186ca82193c48525cb352110d8f
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-02-19 22:35:20 +0530

    Update EmployeeService to use production API endpoint

--- frontend/src/service/EmployeeService.js
+++ frontend/src/service/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const EMPLOYEE_S_API_BASE_URL = "http://localhost:9090/employees"
+const EMPLOYEE_S_API_BASE_URL = "https://teamtrack-ol21.onrender.com/employees"
 class EmployeeService{
     saveEmployee(employee){
         return axios.post(EMPLOYEE_S_API_BASE_URL, employee)
